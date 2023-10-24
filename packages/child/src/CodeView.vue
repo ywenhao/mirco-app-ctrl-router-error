70c8b28d2c6fb053a010d30ea15dda36c672045f
actions.win <actions.win@foxmail.com>
2023-10-24 15:42:33 +0800

    feat: 添加child 修改title

--- packages/child/src/CodeView.vue
+++ packages/child/src/CodeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, watchPostEffect } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const type = computed(() => route.params.type as string)
+
+watchPostEffect(() => {
+  document.title = `child - ${type.value}`
+})
 </script>
 
 <template>code: {{ type }}</template>
